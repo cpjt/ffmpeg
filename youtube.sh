84814657aee5941a9552fa1a9a350c28d6bd6970
cpjt <40307004+cpjt@users.noreply.github.com>
2018-07-23 23:35:12 -0300

    Update youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -1,6 +1,6 @@
 VBR="200k"                                    
-FPS="20"                                    
-QUAL="ultrafest"                              
+FPS="15"                                    
+QUAL="veryfast"                              
 YOUTUBE_URL="rtmp://104.196.20.47:1935/live"
 
 SOURCE="http://www.blogger.com/video-play.mp4?contentId=639b98419fa75c5a"           
